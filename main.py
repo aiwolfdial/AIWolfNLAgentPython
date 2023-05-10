c89324d0548063a3f88dc1bccdc093e69f9c0b68
navleorange <navleorange.0718@gmail.com>
2023-05-10 14:17:17 +0900

    fix: change c.ini file name

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def main(config_path:str, name:str):
     client.close()
 
 if __name__ == "__main__":
-    config_path = "./res/client.ini"
+    config_path = "./res/config.ini"
 
     inifile = lib.util.check_config(config_path=config_path)
     inifile.read(config_path,"UTF-8")
