3152e9c08640c2958921fcde24d3d865cfb4b316
navleorange <navleorange.0718@gmail.com>
2023-12-17 01:16:46 +0900

    fix :remote port

--- connect_to_server.py
+++ connect_to_server.py
@@ -1,5 +1,12 @@
 import lib
-from lib.connection import TCPClient
+from lib.connection import TCPClient,SSHServer
+
+def get_bind_port(inifile) -> map:
+	parser = SSHServer(inifile=inifile,name=inifile.get("agent","name1"))
+	parser.set_ssh_toolkit()
+	parser.set_ssh_config()
+
+	return parser
 
 if __name__ == "__main__":
 	config_path = "./res/config.ini"
@@ -7,4 +14,16 @@ if __name__ == "__main__":
 	inifile.read(config_path,"UTF-8")
 	client = TCPClient(inifile=inifile)
 
+	parser = get_bind_port(inifile=inifile)
+	port_list = []
+
+	for remote_foward in parser.config["remoteforward"]:
+		parts = remote_foward.split()
+		remote_port = parts[0]
+		port_list.append(remote_port)
 	
+	client.connect()
+
+	client.send(" ".join(port_list))
+
+	client.close()
---
+++ execute.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python3 multiprocess.py &
+sleep 3
+python3 connect_to_server.py
