652699a2b9b043eeb5842daa2199c3d7956df7dc
navleorange <navleorange.0718@gmail.com>
2023-12-17 17:35:16 +0900

    add: ja description

--- connect_to_server.py
+++ connect_to_server.py
@@ -1,29 +1,43 @@
+import time
 import lib
-from lib.connection import TCPClient,SSHServer
-
-def get_bind_port(inifile) -> map:
-	parser = SSHServer(inifile=inifile,name=inifile.get("agent","name1"))
-	parser.set_ssh_toolkit()
-	parser.set_ssh_config()
-
-	return parser
+from lib.connection import SSHServer,TCPClient
+from sshtunnel import SSHTunnelForwarder
 
 if __name__ == "__main__":
 	config_path = "./res/config.ini"
 	inifile = lib.util.check_config(config_path=config_path)
 	inifile.read(config_path,"UTF-8")
-	client = TCPClient(inifile=inifile)
+	client = SSHServer(inifile=inifile,name=inifile.get("agent","name1"))
+	config = client.read_ssh_config()
 
-	parser = get_bind_port(inifile=inifile)
 	port_list = []
 
-	for remote_foward in parser.config["remoteforward"]:
+	for remote_foward in config["remoteforward"]:
 		parts = remote_foward.split()
 		remote_port = parts[0]
 		port_list.append(remote_port)
-	
-	client.connect()
 
-	client.send(" ".join(port_list))
+	server = SSHTunnelForwarder(
+		config["hostname"],
+		ssh_username=config["user"],
+		local_bind_address=("localhost",10001),
+		remote_bind_address=("127.0.0.1",10001),
+		allow_agent=True,
+	)
+
+	server.start()
+	
+	try:
+		client = TCPClient(inifile=inifile)
+		client.connect()
+
+		time.sleep(1)
+
+		client.send(message=" ".join(port_list))
+	except:
+		print("ERROR")
 
 	client.close()
+
+	server.stop()
+	server.close()
