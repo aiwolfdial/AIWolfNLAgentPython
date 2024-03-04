28f6fcb05c5a73f3aa437b52aae7c1fd5830c38e
navleorange <navleorange.0718@gmail.com>
2024-03-04 10:49:08 +0900

    add: keep connection

--- multiprocess.py
+++ multiprocess.py
@@ -4,21 +4,23 @@ import main
 import lib
 
 def execute_game(inifile:configparser.ConfigParser, name:str):
-    # connect to server or listen client
-    
-    if inifile.getboolean("connection","ssh_flag"):
-        sock = lib.connection.SSHServer(inifile=inifile, name=name)
-    else:
-        sock = lib.connection.TCPServer(inifile=inifile, name=name) if inifile.getboolean("connection","host_flag") else lib.connection.TCPClient(inifile=inifile)
-    
-    sock.connect()
 
-    received = None
+    while inifile.getboolean("connection","keep_connection"):
 
-    for _ in range(inifile.getint("game","num")):
-        received = main.main(sock=sock, inifile=inifile, received=received, name=name)
-    
-    sock.close()
+        # connect to server or listen client
+        if inifile.getboolean("connection","ssh_flag"):
+            sock = lib.connection.SSHServer(inifile=inifile, name=name)
+        else:
+            sock = lib.connection.TCPServer(inifile=inifile, name=name) if inifile.getboolean("connection","host_flag") else lib.connection.TCPClient(inifile=inifile)
+        
+        sock.connect()
+
+        received = None
+
+        for _ in range(inifile.getint("game","num")):
+            received = main.main(sock=sock, inifile=inifile, received=received, name=name)
+        
+        sock.close()
 
 if __name__ == "__main__":
     config_path = "./res/config.ini"
@@ -29,7 +31,7 @@ if __name__ == "__main__":
     agent_num = int(inifile.get("agent","num"))
 
     print("agent_num:" + str(agent_num))
-
+        
     for i in range(agent_num):
         process = multiprocessing.Process(name="p" + str(i+1), target=execute_game, args=(inifile, inifile.get("agent","name" + str(i+1))))
         process.start()
